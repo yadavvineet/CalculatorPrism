d37a81645e72d78a3121afde39749859828a2097
Vineet Yadav <vineet.yadav@nagarro.com>
2016-05-15 02:08:52 +0530

    Minor Refactoring

--- CalculatorWithPrism/CalculatorWithPrism/ViewModels/MainWindowViewModel.cs
+++ CalculatorWithPrism/CalculatorWithPrism/ViewModels/MainWindowViewModel.cs
@@ -9,21 +9,50 @@ using Microsoft.Practices.Prism.Mvvm;
 
 namespace CalculatorWithPrism.ViewModels
 {
+    /// <summary>
+    /// Class MainWindowViewModel.
+    /// </summary>
+    /// <seealso cref="Microsoft.Practices.Prism.Mvvm.BindableBase" />
     public class MainWindowViewModel : BindableBase
     {
-        private string _statusString;
+        #region Fields
 
+        /// <summary>
+        /// The _status string
+        /// </summary>
+        private string _statusString;
+        #endregion
+
+        #region Public Properties
+        /// <summary>
+        /// Gets or sets the status string.
+        /// </summary>
+        /// <value>The status string.</value>
         public string StatusString
         {
             get { return _statusString; }
             set { SetProperty(ref _statusString, value); }
         }
+        #endregion
 
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+        /// </summary>
         public MainWindowViewModel()
         {
             StatusString = $"Calulcator Version {GetVersionNumber()} (c) vineetyadav.com";
         }
-        
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Gets the version number.
+        /// </summary>
+        /// <returns>System.String.</returns>
         private string GetVersionNumber()
         {
             var assembly = Assembly.GetExecutingAssembly();
@@ -31,5 +60,6 @@ namespace CalculatorWithPrism.ViewModels
             var version = fvi.FileVersion;
             return version;
         }
+        #endregion
     }
 }
